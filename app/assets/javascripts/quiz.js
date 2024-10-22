d058fa35c10d596efb38376bb697f0e9196e3ca3
Ukyo-Nawano <ukyoriderdrive42107@ezweb.ne.jp>
2024-10-22 16:38:44 +0900

    add:押せない挙動

--- app/assets/javascripts/quiz.js
+++ app/assets/javascripts/quiz.js
@@ -1,5 +1,5 @@
 "use strict";
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
     const quizzes = document.querySelectorAll('[data-quiz-id]');
 
     quizzes.forEach(quiz => {
@@ -9,22 +9,18 @@ document.addEventListener("DOMContentLoaded", function() {
         const correctChoice = quiz.querySelector('.choice[data-is-valid="true"]');
 
         choices.forEach(choice => {
-            choice.addEventListener('click', function() {
-                // すでに選択されている場合は何もしない
+            choice.addEventListener('click', () => {
                 if (quiz.querySelector('.choice.selected')) return;
 
-                // 正解かどうかを判断
                 const isValid = choice.getAttribute('data-is-valid') === 'true';
                 const correctChoiceSpan = correctContainer.querySelector('.correct_choice');
                 const correctAnswerSpan = incorrectContainer.querySelector('.correct_answer');
 
-                // すべての選択肢を非活性にする
                 choices.forEach(c => {
-                    c.classList.add('selected'); // すでに選択された選択肢としてマーク
-                    c.style.opacity = '0.5'; // 透明度を0.5にする
+                    c.classList.add('selected');
+                    c.style.opacity = '0.5';
                 });
 
-                // 正解・不正解の表示を制御
                 correctContainer.classList.add('hidden');
                 incorrectContainer.classList.add('hidden');
 
@@ -33,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     correctChoiceSpan.textContent = choice.textContent;
                 } else {
                     incorrectContainer.classList.remove('hidden');
-                    correctAnswerSpan.textContent = correctChoice.textContent; // 正解選択肢を表示
+                    correctAnswerSpan.textContent = correctChoice.textContent;
                 }
             });
         });
